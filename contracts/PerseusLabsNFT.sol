f366f9ab19301e9f19c558be99d2c87f75f17e02
decave27 <decave27@gmail.com>
2022-03-20 13:08:05 +0900

    feat: add ownable contract

--- contracts/PerseusLabsNFT.sol
+++ contracts/PerseusLabsNFT.sol
@@ -1,16 +1,8 @@
 pragma solidity ^0.5.6;
 
-import "@klaytn/contracts/token/KIP17/KIP17Full.sol";
-import "@klaytn/contracts/token/KIP17/KIP17Mintable.sol";
-import "@klaytn/contracts/token/KIP17/KIP17MetadataMintable.sol";
-import "@klaytn/contracts/token/KIP17/KIP17Burnable.sol";
-import "@klaytn/contracts/token/KIP17/KIP17Pausable.sol";
+import "@klaytn/contracts/token/KIP17/KIP17Token.sol";
+import "./Ownable.sol";
 
-contract KIP17TokenOwnable is KIP17Full, KIP17Mintable, KIP17MetadataMintable, KIP17Burnable, KIP17Pausable {
-    constructor (string memory name, string memory symbol) public KIP17Full(name, symbol) {
-    }
-}
-
-contract PerseusLabsNFT is KIP17TokenOwnable("PerseusLabsNFT", "PSERLAB") {
+contract PerseusLabsNFT is Ownable, KIP17Token("PerseusLabsNFT", "PSERLAB") {
 
 }
