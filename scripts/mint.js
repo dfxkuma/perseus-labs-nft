3645bf354736257bff9b3ebc22287954d9eb5907
decave27 <decave27@gmail.com>
2022-03-20 13:45:29 +0900

    feat: add mintWithTokenURI script

--- scripts/mint.js
+++ scripts/mint.js
@@ -4,7 +4,7 @@ async function main() {
     const contract = await hre.ethers.getContractFactory("PerseusLabsNFT");
     const deployedContract = await contract.attach(process.env.DEPLOYED_CONTRACT)
 
-    let metadataURI = "ipfs://CID";
+    let metadataURI = "https://example.com";
     let [deployer] = await hre.ethers.getSigners();
     const mintTx = await deployedContract.mintWithTokenURI(deployer.address, 0, metadataURI);
 
